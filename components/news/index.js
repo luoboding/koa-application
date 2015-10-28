c1a119bc729ed01a5d4dbc0283a9d5137206bec2
bonedeng <bonedeng@bonedengs-MacBook-Pro.local>
2015-10-28 10:52:06 +0800

    [dengpeng]: remove unused file

--- components/news/index.js
+++ components/news/index.js
@@ -4,12 +4,5 @@ var route = require('koa-route');
 
 module.exports = function (rootApp) {
   var show = require('./controllers/news-view-ctrl')(rootApp);
-  // console.log('show is ', show);
-  // function *show(id) {
-  //   var post = posts[id-1];
-  //   if (!post) this.throw(404, 'invalid post id');
-  //   this.body = yield render('show', { post: post });
-  // }
-
   rootApp.use(route.get('/post/:id', show));
 }
